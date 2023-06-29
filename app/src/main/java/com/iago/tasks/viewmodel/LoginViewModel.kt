56091c21ac49a21d3e60731dca88eeecc6eabbf4
Iago Gondim <iago-gondim@hotmail.com>
2023-06-29 15:42:17 -0300

    add date of authentication login

--- app/src/main/java/com/iago/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/iago/tasks/viewmodel/LoginViewModel.kt
@@ -4,14 +4,19 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.iago.tasks.service.constants.TaskConstants
 import com.iago.tasks.service.listener.APIListener
 import com.iago.tasks.service.model.PersonModel
 import com.iago.tasks.service.model.ValidationModel
 import com.iago.tasks.service.repository.PersonRepository
+import com.iago.tasks.service.repository.SecurityPreferences
+import com.iago.tasks.service.repository.remote.RetrofitClient
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
   
   private val personRepository = PersonRepository(application.applicationContext)
+  private val securityPreferences = SecurityPreferences(application.applicationContext)
+  
   
   private val _login = MutableLiveData<ValidationModel>()
   val login: LiveData<ValidationModel> = _login
@@ -23,6 +28,12 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
   fun doLogin(email: String, password: String) {
     personRepository.login(email, password, object : APIListener<PersonModel> {
       override fun onSuccess(result: PersonModel) {
+        securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
+        securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
+        securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
+        
+        RetrofitClient.addHeaders(result.token, result.personKey)
+        
         _login.value = ValidationModel()
       }
       
